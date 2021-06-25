c5dd6129df1566963ce30c01447a6a0c83eabc0f
Julianeme <juliantve@gmail.com>
2021-06-25 16:42:31 -0500

    save and to_dict  methods added

--- models/base_model.py
+++ models/base_model.py
@@ -3,25 +3,49 @@
 import uuid
 import datetime
 
+
 class BaseModel():
 
-    def __init__(self, id, created_at, updated_at):
-        self.created_at = datetime.datetime.strptime(
-            '2020-06-19T15:52:50Z', "%Y-%m-%dT%H:%M:%SZ")
-        self.updated_at = datetime.datetime.strptime(
-            '2020-06-19T15:52:50Z', "%Y-%m-%dT%H:%M:%SZ")
+    def __init__(self):
+        '''
+        * created_at: assign with the current datetime when an instance is created
+        * updated at: assign with the current datetime when an instance is created
+        and it will be updated every time you change your object
+        * id: assign with an uuid when an instance is created
+        '''
+        self.created_at = str(datetime.datetime.strptime(
+            '2016-10-06 15:14:54.322989', '%Y-%m-%d %H:%M:%S.%f'))
+        self.updated_at = str(datetime.datetime.strptime(
+            '2016-10-06 15:14:54.322989', '%Y-%m-%d %H:%M:%S.%f'))
         self.id = str(uuid.uuid4())
 
     def __str__(self):
         '''str magic method returns the characteristics of the object'''
         return ("[{}] ( {} {})".format(type(self).__name__, self.id, self.__dict__))
 
+    def save(self):
+        '''updates the public instance attribute updated_at with the current datetime'''
+        self.updated_at = str(datetime.datetime.strptime(
+            '2016-10-06 15:14:54.322989', '%Y-%m-%d %H:%M:%S.%f'))
+        print("HOLAHOLAHOLA: {}".format(self.updated_at))
 
-my_model = BaseModel(5, 5, 5)
-print(my_model.id)
-print(my_model.created_at)
-print(my_model.updated_at)
+    def to_dict(self):
+        '''returns a dictionary containing all keys/values of __dict__ of the instance'''
+        return self.__dict__
+
+my_model = BaseModel()
+my_model.name = "Holberton"
+my_model.my_number = 89
 print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key,
+          type(my_model_json[key]), my_model_json[key]))
+
 
 
 
