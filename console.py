f3250eeed8f233ea62c5d706da1abe86da1f95ab
sergiovelasquez18 <2458@holbertonschool.com>
2021-06-25 15:24:32 -0500

    console

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""console"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """HBNBcosole"""
+    prompt = "(hbnb) "
+
+    def do_quit(self, args):
+        """Ayuda del comando1"""
+        print("Hasta pronto")
+        return True
+
+    def do_EOF(self, args):
+        """Ayuda del comando1"""
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
