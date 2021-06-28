776a328d97b88726236e795b6c18270ce88883ae
Esneider Granada Valencia <esneiderimac@iMac-de-Esneider.local>
2021-06-28 16:11:40 -0500

    update

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,14 @@ import uuid
 from datetime import datetime
 
 class BaseModel():
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        if kwargs > 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    pass
+                elif key == 'created_at':
+                    self.created_at = 
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -20,17 +27,30 @@ class BaseModel():
     def to_dict(self):
         my_dict = self.__dict__
         my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
         return my_dict
 
 
 if __name__=='__main__':
-    model1 = BaseModel()
-    model1.name = "Holbie"
-    model1.mynumber = 89
-    print(model1)
-    print("//////////////////////////////////////////////////////////")
-    model1.save()
-    print(model1)
-    print("//////////////////////////////////////////////////////////")
-    my_model_json = model1.to_dict()
+    my_model = BaseModel()
+    my_model.name = "Holberton"
+    my_model.my_number = 89
+    print(my_model.id)
+    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
+    my_model_json = my_model.to_dict()
     print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
