6e66f5b519cec3af27c2c441c6d13895e980dd69
Julianeme <juliantve@gmail.com>
2021-06-29 10:35:35 -0500

    test_base_mode file added

---
+++ tests/test_base_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+adde comment here
+"""
+import unittest
+import uuid
+from datetime import datetime
+import models
+
+
+class Test_Base(unittest.TestCase):
+    """Test class"""
+
+if __name__=='__name__':
+    unittest.main()
