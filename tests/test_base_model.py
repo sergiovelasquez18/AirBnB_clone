4e17c7fa9985e9a04d0213b91746a8d152506125
Julianeme <juliantve@gmail.com>
2021-06-30 23:35:29 -0500

    6 first test added to the test con_methods method in the test_base_model.py file

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -3,13 +3,26 @@
 adde comment here
 """
 import unittest
-import uuid
-from datetime import datetime
 import models
 
 
-class Test_Base(unittest.TestCase):
-    """Test class"""
+class Test_Console(unittest.TestCase):
+    """Console methods test"""
+
+
+def test_con_methods(self):
+    """tests uncompleted console command lines"""
+    self.assertEqual("show", "** class name missing **")
+
+    self.assertEqual("show BaseModel", "** class name missing **")
+
+    self.assertEqual("destroy", "** class name missing **")
+
+    self.assertEqual("destroy BaseModel", "** class name missing **")
+
+    self.assertEqual("update", "** class name missing **")
+
+    self.assertEqual("update BaseModel", "** instance id missing **")
 
 if __name__ == '__name__':
     unittest.main()
