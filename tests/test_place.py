f6029e30c33dc3104e6f5d9c76aa1f2256b9af09
Julianeme <juliantve@gmail.com>
2021-07-01 00:33:31 -0500

    city and place test empty files added

---
+++ tests/test_place.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+adde comment here
+"""
+import unittest
+import models
+
+
+class Test_city(unittest.TestCase):
+    """Console methods test"""
+
+
+def test_con_methods(self):
+    """tests uncompleted console command lines"""
+
+
+if __name__ == '__name__':
+    unittest.main()
